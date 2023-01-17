8e54524f0f81f878fb5c4f3992c73a2962abb1ce
leslie <690379770@qq.com>
2023-01-17 23:39:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SQL Generater
+
+## Project Introduce
+
+This project aims to solve the problem of manual input of large amounts of test data in Japanese development. It can quickly generate SQL and simulate data. 
---
+++ src/test/java/com/leslie/sqlGenerater/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leslie.sqlGenerater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
